95317e9221b82ce6862de8729a48ef394dedec74
SeherSaylik <sehersaylik@users.noreply.github.com>
2021-09-10 11:58:11 +0300

    Auction contract is completed

---
+++ contracts/OpenAuction.sol
@@ -0,0 +1,77 @@
+pragma solidity ^0.8.4;
+
+contract OpenAuction {
+    //statics
+    address public owner;
+    string public _auctionName;
+    uint256 public _minBidPrice;
+
+    //states
+    uint256 public _highestBid;
+    bool public isActive;
+    address payable _bidderAddress;
+
+    // Modifier
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function startAuction(string memory auctionName, uint256 minBidPrice)
+        public
+        onlyOwner
+    {
+        isActive = true;
+        _minBidPrice = minBidPrice;
+        _auctionName = auctionName;
+    }
+
+    function endAuction() public onlyOwner {
+        payable(address(owner)).transfer(_highestBid);
+        isActive = false;
+    }
+
+    function bid() public payable {
+        require(isActive == true, "Auction is not available anymore");
+        uint256 bidPrice;
+        bidPrice = msg.value;
+        require(bidPrice >= _minBidPrice);
+        require(
+            bidPrice >= _highestBid,
+            "Bid price is not bigger than the highest bid price"
+        );
+        _bidderAddress.transfer(_highestBid);
+        _bidderAddress = payable(address(msg.sender));
+        _highestBid = bidPrice;
+    }
+
+    function seeBidDetails()
+        public
+        view
+        returns (
+            string memory auctionStatus,
+            string memory auctionName,
+            uint256 minimumBidPrice,
+            address lastBidderAddress,
+            uint256 lastBidPrice
+        )
+    {
+        string memory status;
+        if (isActive == true) {
+            status = "Auction continues.";
+        } else {
+            status = "Auction ended.";
+        }
+        return (
+            status,
+            _auctionName,
+            _minBidPrice,
+            _bidderAddress,
+            _highestBid
+        );
+    }
+}
