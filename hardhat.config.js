061922d1666f493e04de344c1ba9dc789e6e5530
SeherSaylik <sehersaylik@users.noreply.github.com>
2021-10-05 13:49:29 +0300

    react files are added. Home auction page is created

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,21 @@
 require("@nomiclabs/hardhat-waffle");
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "VF51FI3Duv3wY2OLO8_RuTIvxoIsNPMh";
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const RINKEBY_PRIVATE_KEY = "1e953f2feee97923de0badccf7c514bc7b570d4bc90732789328a0c7ddb622f9";
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
   solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: `https://eth-rinkeby.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`],
+    },
+  },
 };
--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import auctionABI from './artifacts/contracts/OpenAuction.sol/OpenAuction.json';
 import Web3 from 'web3';
-
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:3000"));
-const ffunc= async () =>{
-    const auctionContractABI= auctionABI.abi;
-    const auctionAddress= '0xF31B27AB732c9799Ec9a86c57d893c8aaBEEbc64';
-    const auctionContract = await new web3.eth.Contract(auctionContractABI, auctionAddress)
-    console.log(auctionContract);
-}
-ffunc();
+import './App.css';
 
 /* const addScoreToBC = async (uniqueName, score) =>{
     const web3= new Web3(window.web3.currentProvider)
@@ -24,9 +16,68 @@ ffunc();
     }
    }
    */
+  const web3= new Web3(window.web3.currentProvider);
+
+
 function App(){
-    return(
-        <div>Hello</div>
+   const [bid, setBid]= useState({0: "", 1: "", 2: "", 3: "",4: ""});
+   const [bidPrice, setBidPrice]= useState(0);
+   const [buttonDisable, setButtonDisable]= useState();
+    useEffect(() => {
+        callContract();
+    }, []);
+
+    const callContract= async () =>{
+      const auctionContractABI= auctionABI.abi;
+      const auctionAddress= '0xF31B27AB732c9799Ec9a86c57d893c8aaBEEbc64';
+      const auctionContract = await new web3.eth.Contract(auctionContractABI, auctionAddress)
+      console.log(auctionContract);
+      const defaultAccount = (await web3.eth.getAccounts())[0]
+      const bidDetails= await auctionContract.methods.seeBidDetails().call({from: defaultAccount});
+      console.log(bidDetails);
+      console.log(bidDetails[0])
+      setBid(bidDetails);
+      if (bidDetails[0]==="Auction ended."){
+        setButtonDisable(true);
+      }
+     }
+
+     const makeBid = async ()=>{
+      const auctionContractABI= auctionABI.abi;
+      const auctionAddress= '0xF31B27AB732c9799Ec9a86c57d893c8aaBEEbc64';
+      const auctionContract = await new web3.eth.Contract(auctionContractABI, auctionAddress)
+      const defaultAccount = (await web3.eth.getAccounts())[0]
+      const sendBid = await auctionContract.methods.bid().send({from: defaultAccount, value: bidPrice})
+      if (sendBid){
+        alert("successful transaction")
+      }
+     }
+
+     const handleChange = (e) => {
+      setBidPrice(e.target.value)
+     }
+
+   return(
+        <div className="full-page"> {callContract}
+          <h1 className="title">Open Auction</h1>
+          <div className="auction"> {bid[1]} </div>
+          <br></br>
+          <div className="status">{bid[0]}</div>
+          <br></br>
+          <div className="grid-container">
+            <div className="item1">Minimum Bid Price: {bid[2]} wei</div>
+            <div className="item2">Highest Bid:{bid[4]} wei</div>
+            <div className="item3">Highest Bidder Address: {bid[3]}</div>  
+          </div>
+          <br></br>
+          <div className="grid-container">
+            <div className="bid-exp">Minimum bid that you can offer should be at least {bid[4]} wei </div> 
+            <div>
+            <input type="text" className="input" value={bidPrice} onChange={(e) => handleChange(e)} />
+            <button className="button" disabled= {buttonDisable} onClick={makeBid}>Make Bid</button>            
+            </div>
+          </div>
+        </div>
     )
 }
 
